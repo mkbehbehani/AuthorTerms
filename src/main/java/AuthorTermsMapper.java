80216dfff9f9ab4a157d3d5c3722b7bf960e5921
Mashallah Behbehani <mkbehbehani@gmail.com>
2017-03-20 23:33:02 -0500

    strip old commentary

--- src/main/java/AuthorTermsMapper.java
+++ src/main/java/AuthorTermsMapper.java
@@ -1,42 +1,18 @@
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Mapper;
-import org.apache.hadoop.mapreduce.lib.input.FileSplit;
 
 import java.io.IOException;
 import java.util.Arrays;
 import java.util.List;
 
-/* 
- * To define a map function for your MapReduce job, subclass 
- * the Mapper class and override the map method.
- * The class definition requires four parameters: 
- *   The data type of the input key
- *   The data type of the input value
- *   The data type of the output key (which is the input key type 
- *   for the reducer)
- *   The data type of the output value (which is the input value 
- *   type for the reducer)
- */
 
 public class AuthorTermsMapper extends Mapper<LongWritable, Text, Text, Text> {
 //  extract every author in the dataset - pass 1
-
-
-
-  /*
-   * The map method runs once for each line of text in the input file.
-   * The method receives a key of type LongWritable, a value of type
-   * Text, and a Context object.
-   */
   @Override
   public void map(LongWritable key, Text value, Context context)
       throws IOException, InterruptedException {
 
-    /*
-     * Convert the line, which is received as a Text object,
-     * to a String object.
-     */
     String line = value.toString();
     String[] authorNames = line.split(":::")[1].split("::");
     String publicationTitle = line.split(":::")[2].split("::")[0];
@@ -44,7 +20,7 @@ public class AuthorTermsMapper extends Mapper<LongWritable, Text, Text, Text> {
     TermsCleaner titleCleaner = new TermsCleaner();
     List<String> cleansedTitles = titleCleaner.clean(publicationTitleTerms);
 
-//    for each author in the line create kv pairs of author and every word in the term list
+//    for each author in the line, create kv pairs of author and every word in the term list
     for (String author : authorNames) {
       for (String term : cleansedTitles) {
         if(term.length() > 2) {
--- src/main/java/AuthorTermsReducer.java
+++ src/main/java/AuthorTermsReducer.java
@@ -1,60 +1,32 @@
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Reducer;
+
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashSet;
 import java.util.Set;
 
-import org.apache.hadoop.io.IntWritable;
-import org.apache.hadoop.io.Text;
-import org.apache.hadoop.mapreduce.Reducer;
-
-/* 
- * To define a reduce function for your MapReduce job, subclass 
- * the Reducer class and override the reduce method.
- * The class definition requires four parameters: 
- *   The data type of the input key (which is the output key type 
- *   from the mapper)
- *   The data type of the input value (which is the output value 
- *   type from the mapper)
- *   The data type of the output key
- *   The data type of the output value
- */   
 public class AuthorTermsReducer extends Reducer<Text, Text, Text, Text > {
 
-  /*
-   * The reduce method runs once for each key received from
-   * the shuffle and sort phase of the MapReduce framework.
-   * The method receives a key of type Text, a set of values of type
-   * IntWritable, and a Context object.
-   */
+
   @Override
 	public void reduce(Text key, Iterable<Text> values, Context context)
 			throws IOException, InterruptedException {
-        ArrayList<String> fileInstances = new ArrayList<String>();
-		
-		/*
-		 * For each value in the set of values passed to us by the mapper:
-		 */
+        ArrayList<String> termInstancesForAuthor = new ArrayList<String>();
+
 		for (Text value : values) {
-		  
-		  /*
-		   * Add the value to the word count counter for this key.
-		   */
-			fileInstances.add(value.toString());
+			 termInstancesForAuthor.add(value.toString());
 		}
-		Set<String> dedupedFiles = new HashSet<String>(fileInstances);
+		Set<String> dedupedFiles = new HashSet<String>( termInstancesForAuthor);
         ArrayList<String> resultsWithCount = new ArrayList<String>();
         
-        /* Count instances of each file name in the fileInstances, use that to create a total count per file. Store in new ArrayList. */
         for (String fileName : dedupedFiles){
-        	Integer occurancesInFile = Collections.frequency(fileInstances, fileName);
-        	String filenameWithCount = String.format("'%s': %s", fileName, occurancesInFile.toString());
+        	Integer occurancesInFile = Collections.frequency( termInstancesForAuthor, fileName);
+        	String filenameWithCount = String.format("\"%s\": %s", fileName, occurancesInFile.toString());
             resultsWithCount.add(filenameWithCount);
         }
-		/*
-		 * Call the write method on the Context object to emit a key
-		 * and a value from the reduce method. 
-		 */
+
 		context.write(key, new Text(resultsWithCount.toString()));
 	}
 }
